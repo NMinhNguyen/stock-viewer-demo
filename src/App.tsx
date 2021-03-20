a9ec8d956bb08fd16eeb391c7dd582dfdc365a1a
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2021-03-20 01:35:57 +0000

    Extract `StockChart`

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,32 @@ import {
   useContext,
   useEffect,
   useMemo,
-  useReducer,
   useState,
 } from 'react';
 import type { ComponentType, HTMLAttributes } from 'react';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { isAfter, fromUnixTime } from 'date-fns';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/core/Autocomplete';
 import type { AutocompleteProps } from '@material-ui/core/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import ToggleButton from '@material-ui/core/ToggleButton';
-import ToggleButtonGroup from '@material-ui/core/ToggleButtonGroup';
 import Typography from '@material-ui/core/Typography';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import DateFnsAdapter from '@material-ui/lab/AdapterDateFns';
 import type { DateRange } from '@material-ui/lab/DateRangePicker';
 import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
-// @ts-expect-error ts(2307) There's no typings for `react-charts`
-import { Chart } from 'react-charts';
 import { useErrorHandler } from 'react-error-boundary';
 import { FixedSizeList } from 'react-window';
 import type { ListChildComponentProps } from 'react-window';
-
 import { createFilterOptions } from './createFilterOptions';
 import { DateRangePicker } from './DateRangePicker';
 import { ErrorBoundary } from './ErrorBoundary';
 import { useRemoteData } from './useRemoteData';
-import { fetchCandle, fetchStockSymbols } from './api';
-import type { Candle, StockSymbol } from './api';
+import { fetchStockSymbols } from './api';
+import type { StockSymbol } from './api';
+import { StockChart } from './StockChart';
 
 // Disable virtualisation due to
 // 1. terrible UX when the list is 26k items long
@@ -47,17 +41,6 @@ import type { Candle, StockSymbol } from './api';
 // https://github.com/mui-org/material-ui/issues/25417
 // However, I've left the virtualisation code in to show that I've considered it as an option.
 const USE_VIRTUALISATION = false;
-
-const PRICE_PROPERTY_BY_KEY = {
-  OPEN: 'o',
-  HIGH: 'h',
-  LOW: 'l',
-  CLOSE: 'c',
-} as const;
-
-type PriceKey = keyof typeof PRICE_PROPERTY_BY_KEY;
-
-const priceKeys = Object.keys(PRICE_PROPERTY_BY_KEY) as PriceKey[];
 
 const LISTBOX_PADDING = 8; // px
 
@@ -245,143 +228,6 @@ function StockSelect({ symbols, onChange }: StockSelectProps) {
         />
       )}
     />
-  );
-}
-
-type PriceTypeToggleProps = {
-  value: PriceKey;
-  onChange: (value: PriceKey) => void;
-};
-
-function PriceTypeToggle({ value, onChange }: PriceTypeToggleProps) {
-  return (
-    <ToggleButtonGroup value={value} exclusive orientation="vertical">
-      {priceKeys.map((key) => (
-        <ToggleButton
-          key={key}
-          value={key}
-          onClick={() => {
-            onChange(key);
-          }}
-        >
-          {key}
-        </ToggleButton>
-      ))}
-    </ToggleButtonGroup>
-  );
-}
-
-type StockSymbolWithCandle = StockSymbol & Candle;
-
-type ChartProps = {
-  symbols: StockSymbol[];
-  dateRange: DateRange<Date>;
-};
-
-const axes = [
-  {
-    primary: true,
-    type: 'time',
-    position: 'bottom',
-  },
-  {
-    type: 'linear',
-    position: 'left',
-  },
-];
-
-function isValidDateRange(dateRange: DateRange<Date>): dateRange is [Date, Date] {
-  const [start, end] = dateRange;
-  return Boolean(start && end && !isAfter(start, end));
-}
-
-function StockChart({ symbols, dateRange }: ChartProps) {
-  const [priceKey, setPriceKey] = useState<PriceKey>('OPEN');
-
-  const { isIdle, isLoading, error, data, createRemoteDataEffect } = useRemoteData<
-    StockSymbolWithCandle[]
-  >();
-
-  useErrorHandler(error);
-
-  const [chartKey, incrementChartKey] = useReducer((x) => x + 1, 0);
-
-  useEffect(() => {
-    if (!(symbols.length > 0 && isValidDateRange(dateRange))) {
-      return;
-    }
-
-    return createRemoteDataEffect(
-      () =>
-        Promise.all(
-          symbols.map(async (symbol) => {
-            const candle = await fetchCandle(symbol, dateRange);
-            return { ...symbol, ...candle };
-          }),
-        ),
-      {
-        onSuccess: () => {
-          // See comment about `react-charts` below
-          incrementChartKey();
-        },
-      },
-    );
-  }, [dateRange, symbols, createRemoteDataEffect]);
-
-  const chartData = useMemo(() => {
-    if (!data) {
-      return [];
-    }
-
-    return data.map((candle) => ({
-      label: candle.displaySymbol,
-      data: (candle[PRICE_PROPERTY_BY_KEY[priceKey]] ?? []).map((price, index) => ({
-        // If `candle[PRICE_KEYS[priceKey]]` was not null, we can probably assume
-        // `candle.t` is present too.
-        primary: fromUnixTime(candle.t![index]),
-        secondary: price,
-      })),
-    }));
-  }, [data, priceKey]);
-
-  return (
-    <>
-      <div
-        css={css`
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          width: 85%;
-          height: 60%;
-        `}
-      >
-        {
-          // This API seems to be fairly quick,
-          // showing a spinner for a few ms can actually appear jarring.
-          isIdle || isLoading ? null : chartData.length > 0 ? (
-            <>
-              <div
-                css={css`
-                  width: 85%;
-                  height: 100%;
-                `}
-              >
-                <Chart
-                  // `react-charts` seems to have issues with props changing,
-                  // and remounting the component fixes them:
-                  // https://github.com/tannerlinsley/react-charts/issues/134
-                  key={`${priceKey}-${chartKey}`}
-                  data={chartData}
-                  axes={axes}
-                  tooltip
-                />
-              </div>
-              <PriceTypeToggle value={priceKey} onChange={setPriceKey} />
-            </>
-          ) : null
-        }
-      </div>
-    </>
   );
 }
 
