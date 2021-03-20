6d43ab009df14ec25eaf6e10a37fe22fef82efb1
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2021-03-20 04:15:38 +0000

    Add initial test (#10)

---
+++ src/mocks/symbols.ts
@@ -0,0 +1,40 @@
+import type { StockSymbol } from '../api';
+
+export const symbols: StockSymbol[] = [
+  {
+    currency: 'USD',
+    description: 'GAMESTOP CORP-CLASS A',
+    displaySymbol: 'GME',
+    symbol: 'GME',
+  },
+  {
+    currency: 'USD',
+    description: 'AMC ENTERTAINMENT HLDS-CL A',
+    displaySymbol: 'AMC',
+    symbol: 'AMC',
+  },
+  {
+    currency: 'USD',
+    description: 'TESLA INC',
+    displaySymbol: 'TSLA',
+    symbol: 'TSLA',
+  },
+  {
+    currency: 'USD',
+    description: 'NETFLIX INC',
+    displaySymbol: 'NFLX',
+    symbol: 'NFLX',
+  },
+  {
+    currency: 'USD',
+    description: 'FACEBOOK INC-CLASS A',
+    displaySymbol: 'FB',
+    symbol: 'FB',
+  },
+  {
+    currency: 'USD',
+    description: 'ALPHABET INC-CL A',
+    displaySymbol: 'GOOGL',
+    symbol: 'GOOGL',
+  },
+];
